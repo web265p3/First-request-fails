c9cc532d823b293d357e1b7f0035f2de37c421ee
David Elsner <David_elsner777@gmx.de>
2021-01-11 14:58:51 +0100

    Add files via upload

---
+++ Areas/Enterprises/EnterpriseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace DH.Enterprises.WebApi.Areas.Sites
+{
+    [ApiController]
+    public class EnterpriseController : Controller
+    {  
+        [HttpGet]
+        [Authorize]
+        [Route("customers")]
+        public IActionResult GetCustomersAsync()
+        {
+            return Ok("Second request!");
+        }        
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+
+namespace DH.Enterprises.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddMicrosoftIdentityWebApi(Configuration, "AzureAdProtectThisApi")
+                    .EnableTokenAcquisitionToCallDownstreamApi()
+                    .AddInMemoryTokenCaches();
+
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+            services.Configure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options =>
+            {
+                var existingOnTokenValidatedHandler = options.Events.OnTokenValidated;
+                options.Events.OnTokenValidated = async context =>
+                {
+                    await existingOnTokenValidatedHandler(context);
+                    options.Authority = "https://login.microsoftonline.com/common";
+                    options.TokenValidationParameters.RoleClaimType = "roles";
+                };
+            });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Read", policy => policy.RequireRole("Customers.Read.All"));
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.ApplicationName == Environments.Development)
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
